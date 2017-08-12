1b5ee2ac1f752a84c5dae9b843be010d7a46932b
afeiship <1290657123@qq.com>
2017-08-12 10:11:59 +0800

    add regexp

--- src/next-camelize.js
+++ src/next-camelize.js
@@ -3,6 +3,7 @@
   var global = global || this || self || window;
   var nx = global.nx || require('next-js-core2');
   var EMPTY_STR = '';
+  var camelCaseRE = /[-_]+(.)?/g;
 
   nx.camelize = function (inStr) {
     return (inStr || EMPTY_STR).replace(camelCaseRE, function (match, chr) {
