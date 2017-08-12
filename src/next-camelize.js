4c2c18aa5091fae69c5dafb72d112ea0448f7205
afeiship <1290657123@qq.com>
2017-08-12 09:52:57 +0800

    issue: intila

---
+++ src/next-camelize.js
@@ -0,0 +1,17 @@
+(function () {
+
+  var global = global || this || self || window;
+  var nx = global.nx || require('next-js-core2');
+  var EMPTY_STR = '';
+
+  nx.camelize = function (inStr) {
+    return (inStr || EMPTY_STR).replace(camelCaseRE, function (match, chr) {
+      return chr ? chr.toUpperCase() : EMPTY_STR;
+    });
+  };
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = NxCamelize;
+  }
+
+}());
