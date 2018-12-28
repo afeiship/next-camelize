786283cb0956b3bebefbf38fd73d485620d01da3
afeiship <afeiship@gmail.com>
2018-12-28 20:46:55 +0800

    add: pretieer & update version

--- dist/next-camelize.js
+++ dist/next-camelize.js
@@ -1,12 +1,11 @@
-(function () {
-
-  var global = global || this || self || window;
+(function() {
+  var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
   var EMPTY_STR = '';
   var camelCaseRE = /[-_.]+(.)?/g;
 
-  nx.camelize = function (inStr) {
-    return (inStr || EMPTY_STR).replace(camelCaseRE, function (match, chr) {
+  nx.camelize = function(inStr) {
+    return (inStr || EMPTY_STR).replace(camelCaseRE, function(match, chr) {
       return chr ? chr.toUpperCase() : EMPTY_STR;
     });
   };
@@ -14,5 +13,4 @@
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = nx.camelize;
   }
-
-}());
+})();
